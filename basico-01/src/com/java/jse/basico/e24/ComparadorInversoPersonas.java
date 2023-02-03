fe372f989cad8f15d50da6ec129ac093f8fe781d
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-02-02 22:08:30 -0600

    S7E4

---
+++ basico-01/src/com/java/jse/basico/e24/ComparadorInversoPersonas.java
@@ -0,0 +1,13 @@
+package com.java.jse.basico.e24;
+
+import java.util.Comparator;
+
+public class ComparadorInversoPersonas  implements Comparator<Persona> {
+    @Override
+    public int compare(Persona persona1, Persona persona2) {
+        String nombreCompletoPersona1 = persona1.getNombre() + persona1.getApellido();
+        String nombreCompletoPersona2 = persona2.getNombre() + persona2.getApellido();
+
+        return nombreCompletoPersona2.compareTo(nombreCompletoPersona1);
+    }
+}
